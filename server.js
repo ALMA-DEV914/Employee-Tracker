f22a5b59484403af8451ec7a63e46e67e363b704
ALMA_DEV914 <aungonalna58@gamil.com>
2021-12-03 00:18:01 -0800

    Add function to add department data

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+// call the npm package to parse and validate answers on CLI
 const inquirer = require('inquirer');
+// call the database of all data
 const mysql = require('mysql2');
+//call the query for localhost PoRT connection
 const connection = require('./db/connection');
-const apiRoutes = require('./routes/apiRoutes/departmentRoutes');
 
+// Create function for promts of category list
 function categoryList() {
     inquirer.prompt(
         {
@@ -26,7 +29,7 @@ function categoryList() {
             ]
 
         }).then(answer => {
-
+         // call the function that matches the user options or selections
             switch (answer.option) {
                 case "View all departments":
                     viewAllDepartments();
@@ -78,32 +81,38 @@ function categoryList() {
             }
         });
 };
-
+// Create function to view or print the table for all department
 function viewAllDepartments() {
+    // select the data of table department
     const sql = 'SELECT * FROM Department';
     connection.query(sql, (err, res) => {
             if (err) {
                 throw err;
             }
+            //show the table of department
             console.table(res)
+            //return the run lists
             categoryList();
         }
     );
 };
-
+//Create function to view the table roles
 function viewAllRoles() {
    const sql = `Select role.title as Role_title, role.salary as Salary , dept.name as DepartmentName from Role role left join department as dept on dept.id = role.department_id`; 
  connection.query(sql, (err, res) => {
     if (err) {
         throw err;
           }
+          //show the table Roles
         console.table(res)
+        // return the run lists
         categoryList();
         }
     );
 };
-
+// Create function to view table of employees
 function viewAllEmployees(){
+    //Assign the next employee as the manager of the current employee
     const sql = `Select employee.id as EmployeeID, concat(employee.first_name,"  ",employee.last_name ) as EmployeeName , role.title as Job_tittle, role.salary as Salary,dept.name as Department_Name,concat(employee2.first_name,"  ",employee2.last_name) as ManagerName from employee_tracker.employee as employee left join employee_tracker.employee as employee2 on employee2.id=employee.manager_id left join employee_tracker.Role as role on employee.role_id=role.id left join employee_tracker.department as dept on dept.id = role.department_id`;
     connection.query(
         sql, 
@@ -111,17 +120,38 @@ function viewAllEmployees(){
             if (err) {
                 throw err;
             }
+            //show the table of employees
             console.table(res)
+            //return the category lists
             categoryList();
         }
         
     );
 };
+// create a function to add a department
+function addDepartment(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'department',
+            message: 'Please add a department name:'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the department id?'
+        }
 
+    ]).then(answer => {
+        console.log(answer);
+        connection.query('INSERT INTO department SET?', {id: answer.id, name: answer.department}, (err, res) => {
+            if (err) throw err;
+            console.log('Added a new department')
+            categoryList();
+        });
+    });
+};
 
-
-
-function addDepartment(){}
 function addRoles(){}
 function addEmployee(){}
 function updateEmployeeRole(){}
